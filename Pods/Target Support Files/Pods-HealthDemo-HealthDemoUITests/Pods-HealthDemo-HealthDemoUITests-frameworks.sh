b5bf31620c8a47cdcae3ec86ca6d0fc3233b895e
yolandaXcode <1176934256@qq.com>
2023-08-08 11:12:36 +0800

    其他文件提交

--- Pods/Target Support Files/Pods-HealthDemo-HealthDemoUITests/Pods-HealthDemo-HealthDemoUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-HealthDemo-HealthDemoUITests/Pods-HealthDemo-HealthDemoUITests-frameworks.sh
@@ -176,6 +176,7 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/LookinServer/LookinServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WCDB.objc/WCDBObjc.framework"
